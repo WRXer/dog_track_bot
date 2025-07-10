c530169513d290cd7591cedd464ee75a3293562a
STI <wrxerrr@gmail.com>
2025-07-10 10:59:26 +0400

    Добавлены команда start и report

--- main.py
+++ main.py
@@ -8,21 +8,19 @@ load_dotenv()
 
 TOKEN_BOT = os.getenv('TOKEN_BOT')
 bot = AsyncTeleBot(TOKEN_BOT)
+user_data = {}
 
-
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 async def start_handler(message):
+    """Старт"""
     chat_id = message.chat.id
-    await bot.send_message(chat_id, "Start")
+    await bot.send_message(chat_id, "🐾 Бот для помощи безнадзорным животным\n\nЧтобы сообщить о безнадзорном животном:\n1. Нажмите /report\n2. Заполните все обязательные поля\n3. Отправьте местоположение и фото животного\n\nВаша заявка будет передана в службу отлова.")
 
-
-
-
-
-
-
-
-
+@bot.message_handler(commands=['report'])
+async def report_handler(message):
+    """Обработка команды report"""
+    user_data[message.chat.id] = {}
+    await bot.send_message(message.chat.id, "📝 Введите ваше ФИО (полностью):")
 
 
 
